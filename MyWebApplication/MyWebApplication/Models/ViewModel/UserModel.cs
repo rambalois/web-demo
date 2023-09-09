91ae745621abc5c9a422b14bab3861009cdce72c
rambalois <84296721+rambalois@users.noreply.github.com>
2023-09-09 14:31:30 +0800

    commit all files

---
+++ MyWebApplication/MyWebApplication/Models/ViewModel/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApplication.Models.ViewModel
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Created By")]
+        public int CreatedBy { get; set; }
+    }
+}
